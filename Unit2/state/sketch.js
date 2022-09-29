82f72084175c68d23b408483b3de7d352913e2cd
MonoKnight <brboldt@ilstu.edu>
2022-09-29 10:45:13 -0500

    9/29/22

--- Unit2/state/sketch.js
+++ Unit2/state/sketch.js
@@ -1,7 +1,12 @@
 let state = 0;
+let k = 0;
+let cube = 0;
+let x = 0;
 
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
+  textAlign(CENTER);
 }
 
 function draw() {
@@ -10,32 +15,72 @@ function draw() {
   switch (state) {
     case 0:
       background("gray");
-      text("in state 0", 100, 100);
+      for (let j = 0; j < width; j += 25) {
+        for (let i = 0; i < width; i += 35) {
+          fill("red");
+          circle(j + 25, i + 15, 50);
+          fill("orange");
+          circle(j + 25, i + 20, 50);
+          fill("yellow");
+          circle(j + 25, i + 25, 50);
+          fill("green");
+          circle(j + 25, i + 30, 50);
+          fill("blue");
+          circle(j + 25, i + 35, 50);
+          fill("indigo");
+          circle(j + 25, i + 40, 50);
+          fill("violet");
+          circle(j + 25, i + 45, 50);
+        }
+      }
       break;
 
     case 1:
       background("green");
-      text("in state 1", 100, 100);
       for (let j = 0; j < width; j += 25) {
         for (let i = 0; i < width; i += 25) {
-          rect(i, j, 20, 20);
+          push();
+          noStroke();
+          fill(i, j, k);
+          rect(i, j, 25, 25);
+          fill(j, k, i);
+          quad(i + 12.5, j, i + 25, j + 12.5, i + 12.5, j + 25, i, j + 12.5);
+          pop();
         }
       }
       break;
 
     case 2:
-      background("blue");
-      text("in state 2", 100, 100);
+      background("purple");
+      for (let j = 0; j < width; j += 25) {
+        for (let i = 0; i < width; i += 25) {
+          push();
+          fill("blue");
+          rect(i, j, i / 20, j / 20);
+          pop();
+        }
+      }
       break;
 
     case 3:
-      background("purple");
-      text("in state 3", 100, 100);
+      background("black");
+      for (let j = 0; j < width; j += 25) {
+        for (let i = 0; i < width; i += 25) {
+          push();
+          fill("#C4D3DF");
+          circle(i + random(-25, 25), j + (random(-25,25)) , random(1,10));
+          pop();
+        }
+      }
       break;
 
     case 4:
-      background("gold");
-      text("in state 4", 100, 100);
+      for (let j = 0; j < width; j += 50) {
+        for (let i = 0; i < width; i += 10) {
+          fill("black");
+          circle(i, j, 20);
+        }
+      }
       break;
   }
 }
@@ -45,4 +90,6 @@ function mouseReleased() {
   if (state > 4) {
     state = 0;
   }
+  
+  k = random(0,255);
 }
