c022b472d0521a53927ce7bd6740641dbe91a2fe
MonoKnight <brboldt@ilstu.edu>
2022-10-03 10:34:42 -0500

    10/3/22

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,7 +1,49 @@
+let state = 0;
+let timer = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
+  background(220);
 
+  switch (state) {
+    case 0:
+      timer++;
+      background("gray");
+      text("in state 0", 100, 100);
+      if (timer > 10 * 60) {
+        state++;
+        timer = 0;
+      }
+      break;
+
+    case 1:
+      timer++;
+      background("green");
+       text("in state 1", 100, 100);
+      if (timer > 5 * 60) {
+        state++;
+        timer = 0;
+      }
+      break;
+      
+    case 2:
+      timer++;
+      background("blue");
+      text("in state 2", 100, 100);
+      if (timer > 15 * 60) {
+        state = 0;
+        timer = 0;
+      }
+      break;
+  }
+}
+
+function mouseReleased() {
+  state++;
+  if (state > 4) {
+    state = 0;
+  }
 }
