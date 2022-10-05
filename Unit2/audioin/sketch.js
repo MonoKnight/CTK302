6fd98da636ba23fdd9cb5e716ce01e6cede1b398
MonoKnight <brboldt@ilstu.edu>
2022-10-05 09:28:14 -0500

    10/05/22

--- Unit2/audioin/sketch.js
+++ Unit2/audioin/sketch.js
@@ -12,23 +12,30 @@ function setup() {
 }
 
 function draw() {
-  background("green");
+  background("cyan");
 
   // get the sound input
   vol = mic.getLevel(); // returned level is between 0 and 1
 
   // text on the screen for debugging
+  fill("white");
+  textSize((vol * 50) + 10);
+  text("My volume is " + vol.toFixed(3), 10, 150);
   textSize(18);
-  text(
-    "Click the screen first to give\npermission for mic input.\nMy volume is " +
-      vol.toFixed(3),
-    10,
-    60
-  );
+  text("Click the screen first to give\npermission for mic input.", 10, 60);
 
   // this moves the box
   //  x = vol*200 ;
+  fill("green");
   x = map(vol, 0, 1, 0, width);
+  if(x > 75 && x < 150){
+    fill("yellow");
+    text("You are getting a bit loud.", 10, 350)
+  }
+  else if(x >= 200){
+    fill("red");
+    text("You are too loud, quiet down now!", 10, 350);
+  }
   rect(x, 200, 50, 50);
   //gif_createImg.position(x, 200, 50, 50);
 }
