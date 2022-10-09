b4e46c50f427a05e947ee935f902f4ab80c52b29
MonoKnight <brboldt@ilstu.edu>
2022-10-09 01:26:29 -0500

    10/09/22

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,8 +1,14 @@
 let state = 0;
 let timer = 0;
+let i1, i2, i3;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  i1 = loadImage("assets/Sky.jpg");
+  i2 = loadImage("assets/Beef.jpg");
+  i3 = loadImage("assets/Bunny.jpg");
+  imageMode(CENTER);
+  textAlign(CENTER);
 }
 
 function draw() {
@@ -11,9 +17,14 @@ function draw() {
   switch (state) {
     case 0:
       timer++;
-      background("gray");
-      text("in state 0", 100, 100);
-      if (timer > 10 * 60) {
+      background("#97EBF4");
+      push();
+      fill("#D0E6F3");
+      stroke("black");
+      textSize(20);;
+      text("This is a short story about how I enjoyed Parent's Weekend.\nI started the day with a walk around campus. I took a picture of the\nsky because I really liked the pattern the clouds made. It was very\nnice outside so I was able to enjoy my walk.", width/2, 50);
+      image(i1, width/2, 350, 400, 400); 
+      if (timer > 12 * 60) {
         state++;
         timer = 0;
       }
@@ -21,9 +32,10 @@ function draw() {
 
     case 1:
       timer++;
-      background("green");
-       text("in state 1", 100, 100);
-      if (timer > 5 * 60) {
+       background("#f0c0da");
+       text("Later, I met up with my parents and we went shopping\nall around Bloomington. Later on, we all tried Korean BBQ\nfor the first time at a place called Harmony. It was incredible.", width/2, 50);
+       image(i2, width/2, 350, 400, 400); 
+      if (timer > 7 * 60) {
         state++;
         timer = 0;
       }
@@ -31,9 +43,10 @@ function draw() {
       
     case 2:
       timer++;
-      background("blue");
-      text("in state 2", 100, 100);
-      if (timer > 15 * 60) {
+      background("#9e7c37");
+      text("Sadly, I worked night shift that night, so I said goodbye early.\nMy shift itself was boring, but as I was walking home I was able to\nget very close to a bunny. It was a great end to my night.", width/2, 50);
+      image(i3, width/2, 350, 400, 400); 
+      if (timer > 10 * 60) {
         state = 0;
         timer = 0;
       }
@@ -43,7 +56,7 @@ function draw() {
 
 function mouseReleased() {
   state++;
-  if (state > 4) {
+  if (state > 2) {
     state = 0;
   }
 }
