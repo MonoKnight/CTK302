d981adf017a9262e8f4ac9621cb8e08a6bf00acc
MonoKnight <brboldt@ilstu.edu>
2022-11-29 11:38:11 -0600

    11/29/22

--- Unit4/gyrocollector/sketch.js
+++ Unit4/gyrocollector/sketch.js
@@ -65,7 +65,7 @@ function draw() {
     if(fallv > 7) fallv = 0;
     fallt = 0;
   }
-  image(fall[fallv], 0, 0, 50, 50);
+  image(fall[fallv], frogPos.x, frogPos.y, 50, 50);
   ellipse(0, 0, 80, 80);
   pop();
 
